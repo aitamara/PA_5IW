95b13543fadbe68140bbf922e2470e87d20200e8
William BASTARD <bastard@etienne-services.fr>
2022-08-25 03:00:53 +0200

    Ajout Authenticate

--- server/src/model/Model.ts
+++ server/src/model/Model.ts
@@ -1,5 +1,7 @@
 import pg from "pg";
 import dotenv from "dotenv";
+import crypto from "crypto";
+
 dotenv.config();
 
 class Model {
@@ -10,5 +12,10 @@ class Model {
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT,
   });
+  hashIt = (plain)=>{
+    let password = crypto.createHash("sha256").update(plain).digest("hex");
+    return password;
+  }
+  
 }
 export default Model;

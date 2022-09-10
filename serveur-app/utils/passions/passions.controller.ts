e4ec06f7be31067164813b9d4dd1a35608a6dac1
L-Nicolas <nicolas.lacoste.esgi@gmail.com>
2022-09-10 17:57:19 +0200

    Passions presque finis

--- serveur-app/utils/passions/passions.controller.ts
+++ serveur-app/utils/passions/passions.controller.ts
@@ -1,5 +1,8 @@
 import Controller from "../../controller/Controller";
+import ClientModel from "../client/client.model";
 import Passion from "../entity/Passion";
+import QueryResponse from "../interfaces/query.interface";
+import Verification from "../interfaces/verification.interface";
 import PassionsModel from "./passions.model";
 
 export default class PassionsController extends Controller {
@@ -11,19 +14,25 @@ export default class PassionsController extends Controller {
    * @param res
    */
   getPassionsList = async (req, res) => {
+    let code = 400;
+    let message: string = "Bad request";
+    let response: QueryResponse = { error: true, message: message, data: [] };
     try {
-      let data: Array<Passion> = await this.passionMdl.getPassionsList();
-      let message: string = "Aucunes passions";
-      let error: boolean = true;
-      if (data.length > 0) {
-        message = `${data.length} passion(s) récupérée(s)`;
-        error = false;
+      let passions: Array<Passion> = [];
+      let data = await this.passionMdl.getPassionsList();
+      response.message = "Aucunes passions";
+      response.error = data.success ?? true;
+      if (data.data.length > 0) {
+        passions.push(data.data);
+        response.message = `${passions.length} passion(s) récupérée(s)`;
+        response.error = false;
+        response.data = passions;
       }
-      let response: QueryResponse = { error: error, message: message, data: data };
     } catch (error) {
       console.log(error);
       res.status(400).send(error);
     }
+    res.status(code).send(response);
   };
 
   /**
@@ -33,7 +42,41 @@ export default class PassionsController extends Controller {
    * @param res
    */
   updatePassionClient = async (req, res) => {
-    //const response = await PassionsModel.getPassionbyName(req.params.passion_name ?? "")
-    //res.json(response);
+    let code = 400;
+    let message: string = "Bad request";
+    let response: QueryResponse = { error: true, message: message, data: [] };
+
+    if (Object.keys(req.body).length > 0) {
+      let dataIpt: Array<Verification> = [
+        { label: "id_client", type: "number" },
+        { label: "passions", type: "string" },
+      ];
+      let listError = this.verifSecure(dataIpt, req.body);
+
+      if (listError.length > 0) {
+        response.message = "Erreur";
+        response.data.push(listError);
+      } else {
+        try {
+          let cltMdl = new ClientModel();
+          let id_passions: Array<number> = [];
+          let passions: Array<Passion> = [];
+          let data = await this.passionMdl.getPassionsById(id_passions);
+          response.message = "Aucunes passions";
+          response.error = data.success ?? true;
+          if (data.data.length > 0) {
+            passions.push(data.data);
+            let user = await cltMdl.getClientById(+req.params.client_id);
+            data = await this.passionMdl.setPassionsListForClient(user, passions);
+            response.error = false;
+            response.data = passions;
+          }
+        } catch (error) {
+          console.log(error);
+          res.status(400).send(error);
+        }
+      }
+    }
+    res.status(code).send(response);
   };
 }
--- serveur-app/utils/passions/passions.model.ts
+++ serveur-app/utils/passions/passions.model.ts
@@ -1,39 +1,63 @@
 import Model from "../../model/Model";
+import Client from "../entity/Client";
+import Passion from "../entity/Passion";
 
 export default class PassionsModel extends Model {
-  getPassionsList = async function () {
+  private table: string = "passions";
+  private tableWithClient: string = "passions_client";
+  private model: Model;
+
+  /**
+   * Récupération de l'ensemble des passions
+   *
+   * @returns
+   */
+  getPassionsList = async () => {
     try {
-      let { rows } = await model.dbClient.query(`SELECT * FROM public.passions`);
+      let { rows } = await this.model.dbClient.query(`SELECT * FROM ${this.table}`);
       if (rows.length > 0) {
-        return { code: 200, message: "succes", data: rows };
+        return { success: true, message: "Passions récupérées", data: rows };
       } else {
-        return { code: 404, message: "No passions", data: [] };
+        return { success: false, message: "No passions", data: [] };
       }
     } catch (err) {
       console.error(err);
       return { code: 500, message: err, data: [] };
     }
   };
-  getPassionById = async function (passion_id: number) {
+
+  /**
+   * Récupération des passions selon leur id
+   *
+   * @returns
+   */
+  getPassionsById = async (id_passion: Array<number>) => {
     try {
-      let { rows } = await model.dbClient.query(`SELECT * FROM public.passions WHERE id = $1`, [passion_id]);
+      let { rows } = await this.model.dbClient.query(`SELECT * FROM ${this.table}`);
       if (rows.length > 0) {
-        return { code: 200, message: "succes", data: rows };
+        return { success: true, message: "Passions récupérées", data: rows };
       } else {
-        return { code: 404, message: "No passions", data: [] };
+        return { success: false, message: "No passions", data: [] };
       }
     } catch (err) {
       console.error(err);
       return { code: 500, message: err, data: [] };
     }
   };
-  getPassionbyName = async function (passion_name: string) {
+
+  /**
+   * Modification des passions d'un client
+   *
+   * @returns
+   */
+  setPassionsListForClient = async (client: Client, passions: Array<Passion>) => {
     try {
-      let { rows } = await model.dbClient.query(`SELECT * FROM public.passions WHERE passion_name = $1`, [passion_name]);
+      await this.model.dbClient.query(`DELETE * FROM ${this.tableWithClient} WHERE id = $1`, [client.getId]);
+      let { rows } = await this.model.dbClient.query(`INSERT INTO ${this.tableWithClient} `, [client.getId]);
       if (rows.length > 0) {
-        return { code: 200, message: "succes", data: rows };
+        return { success: true, message: "Passions mises à jours", data: rows };
       } else {
-        return { code: 404, message: "No passions", data: [] };
+        return { success: false, message: "Impossible de modifier les passions", data: [] };
       }
     } catch (err) {
       console.error(err);

28df470cb7c7edab81d7658aa0f0e29bbdeb0984
aitamara <aitamarar@gmail.com>
2022-12-10 04:41:58 +0100

    propositions de match

--- serveur-app/utils/propositions/propositions.controller.ts
+++ serveur-app/utils/propositions/propositions.controller.ts
@@ -2,11 +2,15 @@ import { Request, Response } from "express";
 import Controller from "../../controller/Controller";
 import PropositionsModel from "./propositions.model";
 import QueryResponse from "../interfaces/query.interface";
+import Verification from "../interfaces/verification.interface";
 import Client from "../entity/Client";
 import ClientModel from "../client/client.model";
+import { Gender } from "../../constants/Gender";
+import { Here } from "../../constants/Here";
 
 export default class MatchController extends Controller {
     private mdl = new PropositionsModel();
+    private mdlClt = new ClientModel();
 
     /**
    * Récupération des propositions de matchs
@@ -18,17 +22,27 @@ export default class MatchController extends Controller {
     let code = 400;
     let response: QueryResponse = { error: true, message: "Bad request", data: [] };
 
-    try {
-      if(req.session == ""){
+    let dataIpt: Array<Verification> = [{ label: "pro_id", type: "number" }];
+    let listError = this.verifSecure(dataIpt, req.body);
+    let client : Client;
+    client = new Client(100, "Bupont", "mat", "", new Date, "", "", "", "", Gender.FEM, Here.ALL, Gender.FEM);
+    if (listError.length > 0) {
+      response.message = "Erreur";
+      response.data.push(listError);
+    } else {
+      try {
         let cltMdl = new ClientModel();
         let user: Client = req.session.user;
-        let propositions = await this.mdl.getPropositions(user, req.body.pro);
+        let propositions = await this.mdl.getPropositions(client, req.body.pro_id); //remplacer client par user en prod
+        code = 200;
+        response.data = propositions.data;
+        response.message = propositions.message
+      }
+      catch(error) {
+          console.log(error);
+          res.status(400).send(error);
       } 
-    }
-    catch(error) {
-        console.log(error);
-        res.status(400).send(error);
-    }    
+    }   
     res.status(code).send(response);
   };
 }
--- serveur-app/utils/propositions/propositions.model.ts
+++ serveur-app/utils/propositions/propositions.model.ts
@@ -11,25 +11,22 @@ export default class PropositionsModel extends Model {
    *
    * @param client
    */
-  public getPropositions = async (client: Client, pro: number) => {
+  public getPropositions = async (client: Client, pro_id: number) => {
     try {
         let { rows } = await this.model.dbClient.query(
-          `SELECT * 
-          FROM user_client 
-          INNER JOIN community 
-          ON community.pro_id = $1
-          WHERE user_client.id = community.client_id 
+          `SELECT client_id, lastname 
+          FROM community 
+          INNER JOIN user_client 
+          ON user_client.id = community.client_id
+          WHERE community.pro_id = $1
           AND user_client.id != $2
+          AND user_client.activated = true
           AND (user_client.intrested_by = $3 OR user_client.intrested_by = 'ALL') 
-          AND ($4 = user_client.gender OR $4 = 'ALL')
-          AND (user_client.here_for = 'ALL' OR user_client.here_for = $5 OR monHereFor = 'ALL')`,
-          [pro, client.getId, client.getGender, client.getInterested_by, client.getHereFor]
-        );
-        console.log(rows);        
+          AND (user_client.gender = $4 OR 'ALL' = $4)
+          AND (user_client.here_for = 'ALL' OR user_client.here_for = $5 OR 'ALL' = $5)`,
+          [pro_id, client.getId, client.getGender, client.getInterested_by, client.getHereFor]          
+        );      
         if (rows.length > 0) {
-            rows.forEach((e) => {
-            let i = 1; 
-            });
             return { success: true, message: "succes", data: rows };
         } else {
             return { success: false, message: "Aucune proposition trouvée", data: [] };
